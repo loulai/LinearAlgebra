b4104cc154a84b2f7f28337e47818d176b4ea156
loulai <louiselai88@gmail.com>
2015-09-06 15:53:42 +0100

    displays properties beautifully

--- src/EchelonForm.java
+++ src/EchelonForm.java
@@ -3,23 +3,10 @@ public class EchelonForm {
 
 	public static void main(String[] args) {
 		Matrix sampleM = new Matrix(new int[][]{{0, -3, -6, 4, 9}, {-1, -2, -1, 3, 1}, {-2, -3, 0, 3, -1}, {1, 4, 5, -9, -7}});
-		System.out.println(sampleM.isMatrixValid());
+		sampleM.printProperties();
 		
 	}
 
-	public static int[] findPivotPosition(int[][] m){
-		int pivotColumn = 0;
-		int[] pivotPosition = {0, pivotColumn}; //row, column. Row is always top one.
-		for(int col = 0; col < m[0].length; col++) { //loop through COLUMN first, from left to right
-			for(int row = 0; row < m.length; row++){
-				if(m[row][col] != 0){ //any non-zero in that column means the pivot col, coz going left to right in columns
-					pivotColumn = col;
-					break;
-				}
-			}
-		}
-		return pivotPosition;
-	}
 	
 	
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -5,6 +5,7 @@ public class Matrix {
 	private int numOfRows;
 	private int numOfColumns;
 	private boolean isValid = true;
+	private int[] pivotPosition;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,6 +16,28 @@ public class Matrix {
 		this.raw = m;
 		this.numOfRows =  m.length;
 		this.numOfColumns = m[0].length;
+		this.pivotPosition = this.findPivotPosition();
+	}
+	
+	public void printProperties(){
+		System.out.println("-------------------------------------------");
+		printMatrix();
+		System.out.println("Number of rows    : " + this.numOfRows);
+		System.out.println("Number of columns : " + this.numOfColumns);
+		System.out.println("Is valid?         : " + this.isMatrixValid());
+		System.out.printf("Pivot position    : row %d, column %d \n", this.pivotPosition[0], this.pivotPosition[1]);
+		System.out.println("-------------------------------------------");
+	}
+	
+	public void printMatrix(){
+		for(int r = 0; r < this.numOfRows; r++){
+			System.out.print("|");
+			for(int c = 0; c < this.numOfColumns; c++){
+				System.out.printf("%3d ", this.raw[r][c]);
+			}
+			System.out.print("|");
+			System.out.println();
+		}
 	}
 	
 	public boolean isMatrixValid(){
@@ -28,4 +51,19 @@ public class Matrix {
 		return isValid;
 	}
 
+	private int[] findPivotPosition(){
+		int pivotColumn = 0;
+		int[] pivotPosition = {0, pivotColumn}; //row, column. Row is always top one.
+		for(int col = 0; col < this.raw[0].length; col++) { //loop through COLUMN first, from left to right
+			for(int row = 0; row < this.raw.length; row++){
+				if(this.raw[row][col] != 0){ //any non-zero in that column means the pivot col, coz going left to right in columns
+					pivotColumn = col;
+					break;
+				}
+			}
+		}
+		return pivotPosition;
+	}
+	
+	
 }
